af4c93cfa036191b3bbe9fbb82ed2f4e43ee04e7
zizam-haha <zhisam.huang145@gmail.com>
2026-04-22 14:57:49 +0200

    Counties

--- js/_menu.js
+++ js/_menu.js
@@ -1,12 +1,6 @@
-createMenu('Statistik rikstadsval geografi.', [
+createMenu('Statistik riksdagsval geografi.', [
   { name: 'Kom igång!', script: 'startpage.js' },
-  { name: 'Trendspanaren', script: 'trendspanaren.js' },
+  { name: 'Den Geografiska Trenden', script: 'geoTrend.js' },
   { name: 'Regionala skillnader', script: 'regional_analys.js' },
-<<<<<<< HEAD
   { name: 'Stad vs Landsbygd', script: 'tathet_analys.js' }
 ]);
-=======
-  { name: 'Stad vs Landsbygd', script: 'tathet_analys.js' },
-  { name: 'Slutsats', script: 'slutsats.js' }
-]); 
->>>>>>> 779f282ea8ca027a9378a59909c624d8f86d04d1
